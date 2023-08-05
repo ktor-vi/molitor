8926adcf0e81651b0e774359353544883a354692
ktorvi <me@ktorvi.me>
2023-08-05 14:11:36 +0200

    Rush qnd Shell 00

--- C_00/ex05/ft_print_comb.c
+++ C_00/ex05/ft_print_comb.c
@@ -9,50 +9,39 @@
 /*   Updated: 2023/08/02 16:16:31 by vphilipp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+void ft_print_comb(void) {
+  char i;
+  char j;
+  char k;
 
-void ft_print_comb(void)
-{
-	char	i;
-	char	j;
-	char	k;
-
-	i = '0';
-	j = '1';
-	k = '2';
-	while (i <= '7')
-	{
-		while (j <= '8')
-		{
-			while (k <= '9')
-			{
-				if (i == '7')
-				{
-					ft_print(i, j, k);
-				}
-				else
-				{
-					ft_print(i, j, k);
-					write(1, ",  ", 2);
-				}
-				k++;
-			}
-			j++;
-			k = j + 1;
-		}
-		i++;
-		j = i + 1;
-		k = j + 1;
-	}
+  i = '0';
+  j = '1';
+  k = '2';
+  while (i <= '7') {
+    while (j <= '8') {
+      while (k <= '9') {
+        if (i == '7') {
+          ft_print(i, j, k);
+        } else {
+          ft_print(i, j, k);
+          write(1, ",  ", 2);
+        }
+        k++;
+      }
+      j++;
+      k = j + 1;
+    }
+    i++;
+    j = i + 1;
+    k = j + 1;
+  }
 }
 
-void	ft_print(char i, char j, char k)
-{
-	write(1, &i, 1);
-	write(1, &j, 1);
-	write(1, &k, 1);
+void ft_print(char i, char j, char k) {
+  write(1, &i, 1);
+  write(1, &j, 1);
+  write(1, &k, 1);
 }
 
-int	main(void)
-{
-	ft_print_comb();
-}
+int main(void) { ft_print_comb(); }
---
+++ Rush_00/main.c
@@ -0,0 +1,7 @@
+
+
+void rush(int x, int y);
+int main() {
+  rush(5, 1);
+  return 0;
+}
